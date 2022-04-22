5e6a9c47bad16c96e8d72e93dcf74ea465ceda38
phoenixdamla <90147636+derkiner@users.noreply.github.com>
2022-04-22 23:51:39 +0300

    Update index.ts

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,26 +1,60 @@
-import { storage, Context } from "near-sdk-as"
+import { Confession, TypeOfConfession, comments, CommentsPosted } from './model'
 
-// return the string 'hello world'
-export function helloWorld(): string {
-  return 'hello world'
+export function addNewConfession(
+  nickname: string,
+  subject: string,
+  category: string,
+  details: string,
+): Confession {
+  return Confession.addConfession(nickname, category, details, subject)
 }
 
-// read the given key from account (contract) storage
-export function read(key: string): string {
-  if (storage.hasKey(key)) {
-    return `✅ Key [ ${key} ] has value [ ${storage.getString(key)!} ]`
-  } else {
-    return `🚫 Key [ ${key} ] not found in storage. ( ${storageReport()} )`
+export function getConfessionById(id: u32): Confession {
+  return Confession.findConfessionById(id)
+}
+
+export function getConfessions(offset: u32, limit: u32 = 5): Confession[] {
+  return Confession.findConfessions(offset, limit)
+}
+
+export function uptodateConfession(
+  id: u32,
+  type: TypeOfConfession,
+): Confession {
+  return Confession.getConfessionsByIdAndUpdate(id, type)
+}
+
+export function deleteConfession(id: u32): void {
+  Confession.findConfessionById_Terminate(id)
+}
+
+export function newAdvice(text: string, confessionId: u32): void {
+  const advice = new CommentsPosted(text, confessionId)
+  comments.push(advice)
+}
+
+const totalConfession = 5
+export function obtainAdvice(): CommentsPosted[] {
+  const quantityOfAdvice = min(totalConfession, comments.length)
+  const startIndex = comments.length - quantityOfAdvice
+  const outcome = new Array<CommentsPosted>(quantityOfAdvice)
+  for (let i = 0; i < quantityOfAdvice; i++) {
+    outcome[i] = comments[i + startIndex]
   }
+  return outcome
 }
 
-// write the given value at the given key to account (contract) storage
-export function write(key: string, value: string): string {
-  storage.set(key, value)
-  return `✅ Data saved. ( ${storageReport()} )`
-}
-
-// private helper method used by read() and write() above
-function storageReport(): string {
-  return `storage [ ${Context.storageUsage} bytes ]`
+export function grabAdviceByCommentId(
+  id: i32,
+  limit: i32 = 5,
+): CommentsPosted[] {
+  const totalPieceOfAdvice = min(limit, comments.length)
+  const startIndex = comments.length - totalPieceOfAdvice
+  const result = new Array<CommentsPosted>(totalPieceOfAdvice)
+  for (let y = 0; y < totalPieceOfAdvice; y++) {
+    if (comments[y].confessionId == id) {
+      result[y] = comments[y + startIndex]
+    }
+  }
+  return result
 }
