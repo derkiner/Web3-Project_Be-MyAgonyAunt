27e86f845aa34506d8d2e10168a15c2d884d22e9
Phoenix <90147636+derkiner@users.noreply.github.com>
2022-04-25 18:22:28 +0300

    Update 2.use-contract.sh

--- scripts/2.use-contract.sh
+++ scripts/2.use-contract.sh
@@ -1,45 +1,17 @@
 #!/usr/bin/env bash
 
-# exit on first error after this point to avoid redeploying with successful build
-set -e
 
-echo
-echo ---------------------------------------------------------
-echo "Step 0: Check for environment variable with contract name"
-echo ---------------------------------------------------------
-echo
+near call $CONTRACT AddNewConfession '{"nickname":<Client's Preferred Pseudo Name>,"subject":,"category":, "details":}' --accountId YOUR-ACCOUNT.testnet
 
-[ -z "$CONTRACT" ] && echo "Missing \$CONTRACT environment variable" && exit 1
-[ -z "$CONTRACT" ] || echo "Found it! \$CONTRACT is set to [ $CONTRACT ]"
+near view $CONTRACT getConfessionById '{"id":ConfessionId}' --accountId YOUR-ACCOUNT.testnet
 
-echo
-echo
-echo ---------------------------------------------------------
-echo "Step 1: Call 'view' functions on the contract"
-echo
-echo "(run this script again to see changes made by this file)"
-echo ---------------------------------------------------------
-echo
+near view $CONTRACT getConfessions '{"offset":,"limit":}' --accountId YOUR-ACCOUNT.testnet
 
-near view $CONTRACT helloWorld
+near call $CONTRACT updateConfession '{"id":CONFESSION ID,"updates":{"type":"","type":"","description":""}' --accountId YOUR-ACCOUNT.testnet
 
-echo
-echo
+near call $CONTRACT deleteConfession '{"id":Confession Id}' --accountId YOUR-ACCOUNT.testnet
 
-near view $CONTRACT read '{"key":"some-key"}'
+near call $CONTRACT newAdvice '{"text":,"confessionId":CONFESSION ID}' --accountId YOUR-ACCOUNT.testnet
 
-echo
-echo
-echo ---------------------------------------------------------
-echo "Step 2: Call 'change' functions on the contract"
-echo ---------------------------------------------------------
-echo
+$CONTRACT obtainAdvice --accountId YOUR-ACCOUNT.testnet / near call $CONTRACT grabAdviceById '{"id":CONFESSION ID,"limit":}' --accountId YOUR-ACCOUNT.testnet
 
-# the following line fails with an error because we can't write to storage without signing the message
-# --> FunctionCallError(HostError(ProhibitedInView { method_name: "storage_write" }))
-# near view $CONTRACT write '{"key": "some-key", "value":"some value"}'
-near call $CONTRACT write '{"key": "some-key", "value":"some value"}' --accountId $CONTRACT
-
-echo
-echo "now run this script again to see changes made by this file"
-exit 0
