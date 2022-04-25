bcb6ab61fb85c1891b8ee8763e83143d416b6129
Phoenix <90147636+derkiner@users.noreply.github.com>
2022-04-25 19:32:03 +0300

    Update General Use-contract.sh

--- scripts/General Use-contract.sh
+++ scripts/General Use-contract.sh
@@ -7,11 +7,11 @@ near view $CONTRACT getConfessionById '{"id":ConfessionId}' --accountId YOUR-ACC
 
 near view $CONTRACT getConfessions '{"offset":,"limit":}' --accountId YOUR-ACCOUNT.testnet
 
-near call $CONTRACT updateConfession '{"id":CONFESSION ID,"updates":{"type":"","type":"","description":""}' --accountId YOUR-ACCOUNT.testnet
+near call $CONTRACT updateConfession '{"id":Confession Id,"updates":{"type":"","type":"","description":""}' --accountId YOUR-ACCOUNT.testnet
 
 near call $CONTRACT deleteConfession '{"id":Confession Id}' --accountId YOUR-ACCOUNT.testnet
 
-near call $CONTRACT newAdvice '{"text":,"confessionId":CONFESSION ID}' --accountId YOUR-ACCOUNT.testnet
+near call $CONTRACT newAdvice '{"text":,"confessionId":Confession Id}' --accountId YOUR-ACCOUNT.testnet
 
-$CONTRACT obtainAdvice --accountId YOUR-ACCOUNT.testnet / near call $CONTRACT grabAdviceById '{"id":CONFESSION ID,"limit":}' --accountId YOUR-ACCOUNT.testnet
+$CONTRACT obtainAdvice --accountId YOUR-ACCOUNT.testnet / near call $CONTRACT grabAdviceById '{"id":Confession Id,"limit":}' --accountId YOUR-ACCOUNT.testnet
 
